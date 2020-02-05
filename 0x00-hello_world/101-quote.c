db8e49c90809f110bb53323c153ba8b4eda04793
FirasOued <oued.firas@hotmail.com>
2020-02-05 19:58:03 +0000

    print quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
 /**
- *main - entry piont
- *print string using strlen and write functions
+ *main - entry piont print string
  *Return: 1
  */
+
 int main(void)
 {
-int quote = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", quote);
+int q = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+q);
 return (1);
 }
