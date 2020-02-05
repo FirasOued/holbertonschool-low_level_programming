ecc7113cc92896372d1747357508056f378b0003
FirasOued <oued.firas@hotmail.com>
2020-02-05 13:32:23 +0000

    fixing error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+/**
+ *main - print a text using prinf command
+ *Return: 0
+ */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return(0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
