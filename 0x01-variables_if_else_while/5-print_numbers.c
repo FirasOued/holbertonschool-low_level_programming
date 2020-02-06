b0c0b346f5c22b3c58f27ffd24c89363efb0af84
FirasOued <oued.firas@hotmail.com>
2020-02-06 18:57:43 +0000

    from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry piont
+ *Description: print all single digit numbers start from 0
+ *Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
