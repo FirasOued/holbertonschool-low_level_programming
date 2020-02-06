b029f38c3318150ec6a619ba7066205a6eb835ba
FirasOued <oued.firas@hotmail.com>
2020-02-06 12:58:17 +0000

    display alphab in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - entry piont
+ *Description: dusplay alphabetic in lowercase
+ *Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a' ; c <= 'z'; ++c)
+{
+putchar(c);
+}
+return (0);
+}
