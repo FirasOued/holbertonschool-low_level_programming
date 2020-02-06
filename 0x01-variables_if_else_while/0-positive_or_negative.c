f2f8db86ea234349f63790748b4b701c3a434629
FirasOued <oued.firas@hotmail.com>
2020-02-06 11:32:29 +0000

    random number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ *main - entry piont
+ *Description: assign a random number n and print if it's +,- or zero
+ *Return: 0
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive", n);
+}
+else if (n == 0)
+{
+printf("%d is zero", n);
+}
+else if (n < 0)
+{
+printf("%d is negative", n);
+}
+return (0);
+}
