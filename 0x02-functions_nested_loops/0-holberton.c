96f5393952b3d7567f7d3c5ab92a9afd16a79d1a
FirasOued <oued.firas@hotmail.com>
2020-02-10 10:31:24 +0000

    prints Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - entry piont
+*Description: program that prints Holberton followed by new line
+*Return: 0
+*/
+int main(void)
+{
+printf("Holberton\n");
+return (0);
+
+
+
+
+
+
+}
