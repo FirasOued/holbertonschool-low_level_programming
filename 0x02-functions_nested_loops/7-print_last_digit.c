6a290aaa6df15d92e8056eb46b8847a0481ba31b
FirasOued <oued.firas@hotmail.com>
2020-02-11 17:50:27 +0000

    code fixed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,22 +8,14 @@
 
 int print_last_digit(int n)
 {
-	int p;
+	n = n % 10;
 
 	if (n < 0)
 	{
 		n *= -1;
-		p = n % 10;
-		_putchar(p + '0');
-		return (p);
 	}
-	else
-	{
-		p = n % 10;
-		_putchar(p + '0');
-		return (p);
-	}
-	_putchar('\n');
+	_putchar(n + '0');
+	return (n);
 
 }
 
