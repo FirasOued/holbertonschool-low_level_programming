38feb62cdd66de4280564434d52e0bc9c92af740
FirasOued <oued.firas@hotmail.com>
2020-02-11 11:52:52 +0000

    adds

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+*add - function that adds two integers
+*@n: first integer
+*@k: seconde integer
+*Return: result = n + k
+ */
+
+int add(int n, int k)
+{
+	int result;
+
+	result = n + k;
+	return (result);
+
+}
+
