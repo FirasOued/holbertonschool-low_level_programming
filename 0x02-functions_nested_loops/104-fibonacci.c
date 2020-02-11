4b6e9148f1752ae5005be62ce8105bd4f4f149eb
FirasOued <oued.firas@hotmail.com>
2020-02-11 21:14:40 +0000

    fib

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - main block
+*Description: finds and prints the first 98 Fibonacci numbers
+*starting with 1 and 2 followed by a new line
+*Return: 0
+*/
+
+int main(void)
+
+{
+
+
+	return (0);
+}
+
