2c8d53174b4b70979a0db8d0be4ad11893e991a5
FirasOued <oued.firas@hotmail.com>
2020-02-12 20:11:31 +0000

    fibo even total

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 
 /**
-* main - main block
+*main - main block
 *Description: Print the sum of even Fibonacci numbers
 *Return: 0
 */
 
 int main(void)
-
 {
+	unsigned long n, p, xfib, sum, i;
+
+	n = 1;
+	p = 2;
+	sum = 0;
+
+	for (i = 0 ; i < 50 ; i++)
+	{
+		xfib = n + p;
+		n = p;
+		p = xfib;
+
+		if (xfib % 2 == 0 && xfib < 4000000)
+		{
+			sum += xfib;
+		}
+	}
+	printf("%lu\n", sum);
 	return (0);
 }
