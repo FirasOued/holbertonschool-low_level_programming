c0cc05b1876e45974f1bc18d5c8b7b3169ec23fa
FirasOued <oued.firas@hotmail.com>
2020-02-12 11:29:04 +0000

    fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
-
 /**
 *main - main block
-*Description: Print the first 50 fibonacci numbers, starting with 1 and 2
+*Description: Print the first 50 fibonacci numbers, starting 1,2
 *Numbers must be coma and space separated
 *Return: 0
 */
+
 int main(void)
 {
-	long long int n, p, xfib;
+	long int n, p, xfib;
 	int i;
 
 	n = 1;
 	p = 2;
-
-	printf("%lli, ", n);
-	printf("%lli, ", p);
+	printf("%li, ", n);
+	printf("%li, ", p);
 
 	for (i = 3 ; i <= 50 ; i++)
 	{
 		xfib = n + p;
 		n = p;
 		p = xfib;
-
 		if (i != 50)
-			printf("%lli, ", xfib);
+			printf("%li, ", xfib);
 		else
-			printf("%lli", xfib);
+			printf("%li", xfib);
+
 	}
 	printf("\n");
 	return (0);
+
 }
