543e76a08847e57e44153bbde1ce82ef13bf5ac9
FirasOued <oued.firas@hotmail.com>
2020-02-12 11:00:01 +0000

    fibonacci 1,2

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,11 +6,28 @@
 *Numbers must be coma and space separated
 *Return: 0
 */
-
 int main(void)
-
 {
+	long long int n, p, xfib;
+	int i;
 
+	n = 1;
+	p = 2;
 
+	printf("%lli, ", n);
+	printf("%lli, ", p);
+
+	for (i = 3 ; i <= 50 ; i++)
+	{
+		xfib = n + p;
+		n = p;
+		p = xfib;
+
+		if (i != 50)
+			printf("%lli, ", xfib);
+		else
+			printf("%lli", xfib);
+	}
+	printf("\n");
 	return (0);
 }
