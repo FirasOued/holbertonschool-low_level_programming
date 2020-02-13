351abb55820b119caf26e7cb182da55341f99616
FirasOued <oued.firas@hotmail.com>
2020-02-13 18:06:38 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+*_isdigit - function that checks for a digit ( 0 throught 9)
+*@c: the digit to be checked
+*Return: 1 if c is a digit, 0 otherwise
+*/
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '0')
+		return (1);
+	else
+		return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+*mul - function that multiplies two integers
+*@a: first integer
+*@b: seconde integer
+*Return: result
+*/
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+
+}
