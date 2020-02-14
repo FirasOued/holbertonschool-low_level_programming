6665a6e53a5e22e58caa0d42169d323617f940d2
FirasOued <oued.firas@hotmail.com>
2020-02-14 22:22:42 +0000

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,8 +7,36 @@
 
 void print_number(int n)
 {
-	
+	unsigned int k1, k2;
+
+	int i, j, achar;
 
 
+
+	i = 1;
+	achar = 1;
+
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+
+	k1 = n;
+	k2 = n;
+
+	while (k1 >= 10)
+	{
+		k1 = k1 / 10;
+		i++;
+	}
+	for (j = 1; j < i; j++)
+		achar = achar * 10;
+	while (achar > 1)
+	{
+		_putchar((k2 / achar) % 10 + '0');
+		achar /= 10;
+	}
+	_putchar(k2 % 10 + '0');
 }
 
