64fef90e2784b4f4d39e2e63b61f955a06b49000
FirasOued <oued.firas@hotmail.com>
2020-02-18 21:26:16 +0000

    an idea for a solution

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,26 +2,27 @@
 
 /**
 *_atoi - function that convert a stringto an integer
-*@s : pionter that pionts to string
+*@s: pionter that pionts to string
 *Return: integer
 */
 
 int _atoi(char *s)
 {
-	int i, int_num;
-	short sign, catch_it;
+	int i;
+	int k, sign;
 
 
-	i = int_num = 0;
-	catch_it = 0;
-	sign = 1;
-
-	while (s[i] != '\0')
+	k = 0;
+	sign = -1;
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == '-')
 			sign *= -1;
-		if (s[i] >= '0' && s[i] <= '9')
+
+		if (s[i] > 47 && s[i] < 58)		
 		{
-
-
-} 
+			k = 0;
+		}
+	}
+	return (k);
+}
