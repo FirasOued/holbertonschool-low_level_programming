abbaf33c784de231f2ee7432f3ebae41d6a5af28
FirasOued <oued.firas@hotmail.com>
2020-02-20 21:12:05 +0000

    try solution

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+**infinite_add - function that adds two numbers
+*@n1: pointer point to string
+*@n2: pointer pointto string
+*@r: pointer point to string
+*@size_r: int
+*Return: string
+*/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i;
+}
