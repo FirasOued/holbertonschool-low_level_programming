eda9abc4ef47a8eff122c92d9bf32fa16681d78c
FirasOued <oued.firas@hotmail.com>
2020-02-20 19:00:26 +0000

    more function

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,43 +8,21 @@
 
 char *cap_string(char *s)
 {
-	int i;
 
-	for (i = 0 ; s[i] != '\0' ; i++)
+	int i = 1;
+	int j;
+	char aft[] = ",;.!?(){}\n\t\" ";
+
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= 32;
+
+	while (s[i] != '\0')
 	{
-		if (i == 0)
-		{
-			if (s[i] >= 'a' && s[i] <= 'z')
-				s[i] = s[i] - 32;
-			continue;
-		}
-		if (s[i] == ' ' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!')
-		{
-			++i;
-			if (s[i] >= 'a' && s[i] <= 'z')
-			{
-				s[i] = s[i] - 32;
-				continue;
-			}
-		}
-		else if (s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')')
-		{
-			++i;
-			if (s[i] >= 'a' && s[i] <= 'z')
-			{
-				s[i] = s[i] - 32;
-				continue;
-			}
-		}
-		else if (s[i] == '{' || s[i] == '}' || s[i] == '\t' || s[i] == '\n')
-		{
-			++i;
-			if (s[i] >= 'a' && s[i] <= 'z')
-			{
-				s[i] = s[i] - 32;
-				continue;
-			}
-		}
+		for (j = 0 ; aft[j] != '\0'; j++)
+			if (s[i - 1] == aft[j] && (s[i] >= 'a' && s[i] <= 'z'))
+				s[i] -= 32;
+		i++;
 	}
 	return (s);
 }
