27cc9071b630e1e80319068a886ec25da0011384
FirasOued <oued.firas@hotmail.com>
2020-02-24 20:00:35 +0000

    add function

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -7,3 +7,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 void set_string(char **s, char *to);
+char *_memset(char *s, char b, unsigned int n);
