2ff9b3f2eaf4bcdb31388ee79d2a06f7053032cc
FirasOued <oued.firas@hotmail.com>
2020-02-26 20:26:20 +0000

    more recursion practical prob

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <string.h>
 
 /**
 *is_palindrome - function that check if a string is palindrom or not
@@ -12,7 +11,22 @@ int is_palindrome(char *s)
 	return (checkPalindrome(s, 0));
 }
 
+/**
+*_strlen_recursion - function that returns the lenght of string
+*@s: pointer point to a string
+*Return: the lenght of a string s
+*/
 
+int _strlen_recursion(char *s)
+{
+	if (s[0] != '\0')
+		return (1 + _strlen_recursion(s + 1));
+
+	else
+	{
+		return (0);
+	}
+}
 
 /**
 *checkPalindrome - check if a string is pal
@@ -23,7 +37,7 @@ int is_palindrome(char *s)
 
 int checkPalindrome(char *s, int index)
 {
-	int len = strlen(s) - (index + 1);
+	int len = _strlen_recursion(s) - (index + 1);
 
 	if (s[index] == s[len])
 	{
