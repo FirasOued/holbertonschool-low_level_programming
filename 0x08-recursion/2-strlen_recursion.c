77237ff9bd60b550c1b1c06bf84cf70f6a325353
FirasOued <oued.firas@hotmail.com>
2020-02-26 17:56:42 +0000

    recursion practical problem

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+*_strlen_recursion - function that returns the lenght of string
+*@s: pointer point to a string
+*Return: the lenght of a string s
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (s[0] != '\0')
+		return (1 + _strlen_recursion(s + 1));
+
+	else
+	{
+		return (0);
+	}
+}
