b2dca29c9d21049e458a40bf05fb5ddcd56080e7
FirasOued <oued.firas@hotmail.com>
2020-02-26 17:47:26 +0000

    recursion practical problems

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+*_pow_recursion - function that returns the value of first int raised
+*to the power of seconde int
+*@x: first integer
+*@y: seconde integer
+*Return: value of x rased by the power of y
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	else
+		return (x * _pow_recursion(x, (y - 1)));	
+}
