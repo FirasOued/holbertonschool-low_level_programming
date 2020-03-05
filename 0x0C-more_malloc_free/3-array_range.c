bf80b4088509e41593c29fc0e80a38809e0fb084
FirasOued <oued.firas@hotmail.com>
2020-03-05 19:39:33 +0000

    fiing code

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,6 +13,9 @@ int *array_range(int min, int max)
 	int i;
 	int *p;
 
+	if (min > max)
+		return (NULL);
+
 	p = malloc(sizeof(*p) * (max - min + 1));
 
 	if (p == NULL)
@@ -20,9 +23,6 @@ int *array_range(int min, int max)
 
 	for (i = 0; i < (max - min + 1); i++)
 		p[i] = min + i;
-
-	if (min > max)
-		return (NULL);
 
 	return (p);
 }
