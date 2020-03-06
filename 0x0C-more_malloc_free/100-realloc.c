dce7d2766a49c23fb24254c78a3ed80256b3a032
FirasOued <oued.firas@hotmail.com>
2020-03-06 19:57:58 +0000

    test code

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -34,7 +34,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		p = malloc(new_size);
 		if (!p)
 			return (NULL);
-		for (i = 0; i < new_size; i++)
+		for (i = 0; i < old_size; i++)
 			p[i] = *((char *)ptr + i);
 		free(ptr);
 	}
