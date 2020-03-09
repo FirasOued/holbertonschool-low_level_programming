4deb85838996177f1184c68d4fe5823cf893f601
FirasOued <oued.firas@hotmail.com>
2020-03-09 16:26:52 +0000

    Structures use case

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - data struct
+*@name: dog's name (pointer)
+*@age: dog's age (float)
+*@owner: dog's owner (pointer)
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
