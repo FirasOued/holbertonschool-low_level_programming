65dd660a46964f68378549d4528763ad4f752631
FirasOued <oued.firas@hotmail.com>
2020-03-09 19:36:30 +0000

    test file with compilation error

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,56 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*new_dog - function that create a new dog
+*@name: dog's name (pointer)
+*@age: dog's age (float)
+*@owner: dog's owner (po,iter)
+*Return: NULL on function fail
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int lename, lenown, i;
+	struct do9 *p = NULL;
+
+	lename = 0;
+	while (name[lename])
+		lename++;
+
+	lenown = 0;
+	while (owner[lenown])
+		lenown++;
+
+	p = malloc(sizeof(struct do9));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	(*p).name = malloc(lename * sizeof(p->name));
+
+	if ((*p).name == NULL)
+	{
+		free((*p).name);
+		free(p);
+		return (NULL);
+	}
+	(*p).owner = malloc(lenown * sizeof(p->owner));
+
+	if ((*p).owner == NULL)
+	{
+		free((*p).owner);
+		free(p);
+		return (NULL);
+	}
+	for (i = 0; i <= lename ; i++)
+		(*p).name[i] = name[i];
+
+	(*p).age = age;
+
+	for (i = 0 ; i < lenown ; i++)
+		(*p).owner[i] = owner[i];
+
+	return (p);
+}
