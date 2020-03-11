69578a5b7302154aa7f0a34907a17473040f3a64
FirasOued <oued.firas@hotmail.com>
2020-03-11 20:01:46 +0000

    opcode

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+*main - print opcode
+*@argc: arguments count
+*@argv: arguments value
+*Return: 0 on success
+*/
+
+int main(int argc, char **argv)
+{
+
+	int i, av;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	av = atoi(argv[1]);
+	if (av < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	for (i = 0; i < av; i++)
+	{
+		printf("%02x", ((unsigned char *)main)[i]);
+		printf("\n");
+	}
+	return (0);
+}
+	
